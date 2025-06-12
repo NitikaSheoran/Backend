80402abc557ff8cec74e247af90320ac2ab90ac2
NitikaSheoran <nitikasheoran5@gmail.com>
2025-06-12 13:58:45 +0530

    configuration done

--- src/app.js
+++ src/app.js
@@ -30,4 +30,18 @@ app.use(express.urlencoded({
 
 app.use(cookieParser())
 
+
+
+
+// routes
+import userRouter from './routes/user.routes.js'
+
+
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+// http://localhost:8000/api/v2/users/register
+
+
 export {app}
